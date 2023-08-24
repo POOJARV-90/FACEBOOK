c4792910b89856bddaaaa189a1e7e76fb6f8084b
POOJARV-90 <132448302+POOJARV-90@users.noreply.github.com>
2023-08-23 20:29:37 -0700

    update

---
+++ components/src/App.js
@@ -0,0 +1,17 @@
+import {Routes,Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+       <Routes>
+        
+
+
+       </Routes>
+
+      
+    </div>
+  );
+}
+
+export default App;
